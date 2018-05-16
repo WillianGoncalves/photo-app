a281e123f82f2c939ea4b6170367d9b7f869182c
Willian <w.goncalves91@gmail.com>
2018-05-16 12:59:25 -0300

    Lesson 223 - Implement a custom registration action.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  #uncomment this line after create a Stripe account
+  #devise_for :users, :controllers => { :registrations => 'registrations' }
   devise_for :users
   root "welcome#index"
 end
